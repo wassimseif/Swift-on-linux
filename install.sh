224bf1805a61add1c3edd9e019fd4b68ec896530
Wassim Seifeddine <wassim_seifeddine95@live.com>
2016-12-17 22:26:42 +0200

    Added sudo for non-root users

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ printer "Updating and Upgrading"
 sudo apt-get update &&  sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get install git 
 printer "Installing LLVM repo."
 
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main" >>  /etc/apt/sources.list 
 
 printer "Install dependencies"
@@ -40,23 +40,23 @@ sudo apt-get update && apt-get install -y \
   systemtap-sdt-dev 
 
  printer "Setting Default LLVM to 3.8"
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
 
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
 
 printer "Installing Swift Compiler"
   # Install Swift compiler
-wget https://swift.org/builds/swift-3.0-release/ubuntu1404/swift-3.0-RELEASE/swift-3.0-RELEASE-ubuntu14.04.tar.gz \
-  && tar xzvf swift-3.0-RELEASE-ubuntu14.04.tar.gz \
-  && rm swift-3.0-RELEASE-ubuntu14.04.tar.gz
+sudo wget https://swift.org/builds/swift-3.0-release/ubuntu1404/swift-3.0-RELEASE/swift-3.0-RELEASE-ubuntu14.04.tar.gz \
+  && sudo tar xzvf swift-3.0-RELEASE-ubuntu14.04.tar.gz \
+  && sudo rm swift-3.0-RELEASE-ubuntu14.04.tar.gz
 
-export PATH=/root/swift-3.0-RELEASE-ubuntu14.04/usr/bin:$PATH
+sudo export PATH=/root/swift-3.0-RELEASE-ubuntu14.04/usr/bin:$PATH
 
 echo "export PATH=/root/swift-3.0-RELEASE-ubuntu14.04/usr/bin:$PATH" >> /root/.bashrc
 printer "Testing Swift Compiler"
 swiftc -h
 printer "Configuring Golden Linker"
-rm /usr/bin/ld && ln -s /usr/bin/ld.gold /usr/bin/ld
+sudo rm /usr/bin/ld && ln -s /usr/bin/ld.gold /usr/bin/ld
 
 printer "Installing Dispatch"
 git clone  https://github.com/apple/swift-corelibs-libdispatch.git \
